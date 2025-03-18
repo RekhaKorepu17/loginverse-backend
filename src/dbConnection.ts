ae70385f30c7b3b4bf4b851cddd3612a4e0af305
RekhaKorepu17 <rekha.korepu@everest.engineering>
2025-03-18 11:59:51 +0530

    Add dialect options in database configuration for database connection

--- src/dbConnection.ts
+++ src/dbConnection.ts
@@ -16,6 +16,11 @@ const host = process.env.HOST || '';
 const sequelize = new Sequelize(database, user, password, {
   host: host,
   dialect: 'postgres',
+  dialectOptions: {
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }}
 });
 
 
